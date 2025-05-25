563c6b62f3cd62225dc89df7b37f8722221e465a
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-05-25 19:06:54 +0200

    Polish global router

--- backend/src/main/kotlin/petklinik/backend/GlobalRouter.kt
+++ backend/src/main/kotlin/petklinik/backend/GlobalRouter.kt
@@ -5,11 +5,9 @@ import org.springframework.web.servlet.function.router
 import petklinik.common.renderWelcome
 
 fun globalRouter() = router {
-
     GET("/") {
         ok().contentType(MediaType.TEXT_HTML).body(renderWelcome())
     }
-
     GET("/oups") {
         throw RuntimeException("Expected: controller used to showcase what happens when an exception is thrown")
     }
