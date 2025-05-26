80d552672c6862c3b85e0d967eca0e47e98b2cee
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-05-26 15:04:23 +0200

    Add Spring Boot tests

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -38,6 +38,14 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
 
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks {
--- backend/src/main/kotlin/petklinik/backend/BackendApplication.kt
+++ backend/src/main/kotlin/petklinik/backend/BackendApplication.kt
@@ -1,17 +1,20 @@
 package petklinik.backend
 
+import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Import
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
+// We don't use @SpringBootApplication since we don't need component scanning
 @EnableAutoConfiguration
+@SpringBootConfiguration(proxyBeanMethods = false)
+@Import(Beans::class)
 @EnableCaching
 @EnableTransactionManagement
-@Import(Beans::class)
-class Application
+class BackendApplication
 
 fun main(args: Array<String>) {
-    runApplication<Application>(*args)
+    runApplication<BackendApplication>(*args)
 }
